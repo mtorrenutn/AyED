7c4f3cf2d3c33758fe136e418577c7ee453eb0cb
Matias Torren UTN <mtorren@frba.utn.edu.ar>
2023-08-28 19:13:33 -0300

    correccion 1

--- 1-calcularPI/main.cpp
+++ 1-calcularPI/main.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-double calculoPI() {
-double pi=0.0 ;
-int sig=1;
-int denom=1;
 
-int interacionesmax =1000000;
+double calcularPi() {
+    double pi = 0.0;
+    double termin = 1.0;
+    int den= 1;
+    int icont = 0;
+    
+    while (termin >= 1e-7) {
+        if (icont % 2 == 0) {
+            pi += termin;
+        } else {
+            pi -= termin;
+        }
 
-for (int i=0; i <interacionesmax; i++) {
+        den += 2;
+        termin = 1.0 / den;
+        icont++;
+    }
 
-    pi +=sig *(1.0/denom);
-
-    sig *= -1;
-
-    denom +=2;
-
+    return pi * 4;
 }
 
-    return pi*4;
+int main() {
+  
+    
+    cout << setprecision(6) << fixed;
+    cout << "Pi: " << calcularPi() << endl;
+    
+    return 0;
 }
-
-
- int main(){
- double pi= calculoPI();
-
- cout.precision(7);
- cout<< "el valor de pi es=" <<pi << endl;
- }
 
 
 
